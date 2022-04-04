9b8fedbd3d9b4222c206353e1f57d1032aa21556
Siddhartha <siddpurwar@gmail.com>
2022-04-04 23:05:15 +0530

    Refactoring

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
-const btnEle1 = document.getElementById("btn-1");
-const btnEle2 = document.getElementById("btn-2");
-const btnEle3 = document.getElementById("btn-3");
+const btnItemEls = document.getElementsByClassName("btn-items-el");
 const invoiceBtn = document.getElementById("invoice-btn");
-const removebtn1 = document.getElementById("remove-btn-1");
-const removebtn2 = document.getElementById("remove-btn-2");
-const removebtn3 = document.getElementById("remove-btn-3");
+const removebtns = document.getElementsByClassName("remove-btn");
 const costEles = document.getElementsByClassName("amt");
 const totalEl = document.getElementById("total-el");
 const quantityEls = document.getElementsByClassName("quantity-el");
 
 
 
-let items = [0, 0, 0];
+let quantity = [0, 0, 0];
 let prices = [10, 20, 30];
 let totalAmt = 0;
 
 
-btnEle1.addEventListener("click", function () {
+
+btnItemEls[0].addEventListener("click", function () {
     updateItem(0, 1);
 });
-btnEle2.addEventListener("click", function () {
+btnItemEls[1].addEventListener("click", function () {
     updateItem(1, 1);    
 });
-btnEle3.addEventListener("click", function () {
+btnItemEls[2].addEventListener("click", function () {
     updateItem(2, 1);   
 });
 invoiceBtn.addEventListener("click", function() {
     checkOut();
 })
-removebtn1.addEventListener("click", function () {
+removebtns[0].addEventListener("click", function () {
     updateItem(0, -1);   
 })
-removebtn2.addEventListener("click", function () {
+removebtns[1].addEventListener("click", function () {
     updateItem(1, -1);   
 })
-removebtn3.addEventListener("click", function () {
+removebtns[2].addEventListener("click", function () {
     updateItem(2, -1);    
 })
 
 
+
 function updateItem(item, count) {
-    items[item] = Math.max(0, items[item] + count);
-    quantityEls[item].innerText = `x ${items[item]}`;
+    quantity[item] = Math.max(0, quantity[item] + count);
+    quantityEls[item].innerText = `x ${quantity[item]}`;
     updateCost(item);
 }
 
@@ -49,15 +47,15 @@ function checkOut() {
     alert("Thanks you for Shopping with us!, Your Total is: " + totalAmt)
     totalAmt = 0;
     for (let i = 0; i < 3; i++) {
-        items[i] = 0;
-        quantityEls[i].innerText = `x ${items[i]}`;
+        quantity[i] = 0;
+        quantityEls[i].innerText = `x ${quantity[i]}`;
         costEles[i].innerText = 0;
     }
     totalEl.innerText = 0;
 }
 
 function updateCost(item) {
-    costEles[item].innerText = items[item] * prices[item];
+    costEles[item].innerText = quantity[item] * prices[item];
     totalAmt = getTotal(); 
     totalEl.innerText = totalAmt;
 }
@@ -65,7 +63,7 @@ function updateCost(item) {
 function getTotal() {
     let total = 0;
     for (let i = 0; i < 3; i++) {
-        total += (items[i] * prices[i]);
+        total += (quantity[i] * prices[i]);
     }
     return total
 }
